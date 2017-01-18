537b1571c798ccc5cab96dd6ada94fe9d29a2c97
David Yee <dvyee@ualberta.ca>
2017-01-18 16:09:41 -0700

    Implement assignment requirements
    - Serve files from the ./www/ folder
    
    - Add support for GET operations only
    
    - Add 200, 404, and 405 responses where appropriate
    
    - Add support for html files
    
    - Add support for css files

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #  coding: utf-8 
 import SocketServer
+from urlparse import urlparse
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, David Yee
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,13 +28,64 @@ import SocketServer
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+responses = {
+    200 : "HTTP/1.1 200 OK\r\n",
+    404 : "HTTP/1.1 404 Not Found\r\n",
+    405 : "HTTP/1.1 405 Method Not Allowed\r\n"
+}
+
+content_type_header = "Content-Type: {0}; charset=utf-8\r\n\r\n"
+
+root_uri = "./www/"
 
 class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        request = self.data.split()
+        http_verb = request[0] # GET, POST, PUT, etc.
+        http_path = request[1] # Requested path (ie: /)
+        http_version = request[2] # HTTP/1.1
+        
+        if http_verb == "GET":
+            self.handle_get(http_path)
+        else:
+            self.request.sendall(responses[405])
+    
+    def handle_get(self, url):
+        parsed_uri = urlparse(url)
+        root_path_uri = root_uri + parsed_uri.path
+        
+        if os.path.isdir(root_path_uri):
+            try:
+                file = open(root_path_uri + "/index.html")
+                response = responses[200] + content_type_header.format("text/html")
+                
+                # Output content of file into the response
+                response += "".join(file.readlines())
+                
+                self.request.sendall(response)
+            except IOError:
+                self.request.sendall(responses[404])
+        else:
+            try:
+                ext = os.path.splitext(root_path_uri)[1]
+                file = open(root_path_uri)
+                
+                content_type = None
+                if ext == ".css":
+                    content_type = "text/css"
+                else:
+                    content_type = "text/html"
+
+                response = responses[200] + content_type_header.format(content_type)
+                
+                # Output content of file into the response
+                response += "".join(file.readlines())
+                
+                self.request.sendall(response)
+            except IOError:
+                self.request.sendall(responses[404])
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
